38785f82ae03cfb22ecd684c7ec197799bf77391
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-01-08 15:17:36 -0600

    _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -21,7 +21,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, text}) {
       if (m.text.includes(linkThisGroup2)) return !0;
       if (m.text.includes(linkThisGroup3)) return !0;
     }
-    await this.sendMessage(m.chat, {text: `_*< ANTI-LINK 2 />*_\n\n*[ ℹ️ ] El participante @user envió un enlace de un grupo de WhatsApp, por lo que será eliminado.*`, mentions: [m.sender]}, {quoted: m});
+    await this.sendMessage(m.chat, {text: `_*< ANTI-LINK 2 />*_\n\n*[ ℹ️ ] El participante @user envió un enlace, por lo que será eliminado.*`, mentions: [m.sender]}, {quoted: m});
     if (!isBotAdmin) return m.reply('_*< ANTI-LINK 2 />*_\n\n*[ ℹ️ ] Para que el anti-link funcione correctamente es necesario que el bot sea administrador del grupo.*');
     if (isBotAdmin && bot.restrict) {
       await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
